80516c8a3d6643ec4ba7ca50956a9ffc1630f521
Ljolan <2018214546@qq.com>
2019-09-27 20:20:18 +0800

    创建server-provider提供者模块

---
+++ server-provider/src/main/java/cn/bibi/serverprovider/api/controller/Test.java
@@ -0,0 +1,24 @@
+package cn.bibi.serverprovider.api.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: ljobin
+ * @description:
+ * @author: Mr.Liu
+ * @create: 2019-09-27 17:59
+ **/
+@RestController
+public class Test {
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+
+}
