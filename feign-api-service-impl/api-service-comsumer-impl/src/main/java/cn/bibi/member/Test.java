a51f08df6e868a2accf8774e2808087d729e4f93
Ljolan <2018214546@qq.com>
2019-10-15 22:08:10 +0800

    开启Hystrix断路器 进行服务降级处理 ，Fallback回调

--- feign-api-service-impl/api-service-comsumer-impl/src/main/java/cn/bibi/member/Test.java
+++ feign-api-service-impl/api-service-comsumer-impl/src/main/java/cn/bibi/member/Test.java
@@ -4,6 +4,7 @@ import cn.bibi.feign.FeignTest;
 import cn.bibi.member.api.TestService;
 import cn.bibi.member.entity.Student;
 import cn.bibi.order.entity.Class;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +20,14 @@ public class Test implements TestService {
     @Autowired
     FeignTest feignTest;
     @Override
+    @HystrixCommand(fallbackMethod = "fallback")
     @RequestMapping("/getStudent")
     public Student getStudent() {
         Class clazz = feignTest.getClazz();
         return new Student("刘衍斌在"+clazz.getClassName()+",班号："+clazz.getClassId(),21);
+    }
+    @RequestMapping("/fallback")
+    public Student fallback(){
+        return new Student("null",0);
     }
 }
--- feign-api-service-impl/api-service-provider-impl/src/main/java/cn/bibi/Application2.java
+++ feign-api-service-impl/api-service-provider-impl/src/main/java/cn/bibi/Application2.java
@@ -3,6 +3,7 @@ package cn.bibi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrix
 public class Application2
 {
     public static void main( String[] args )
